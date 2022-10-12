4235e966cf79f0c921346f63262afca29959b658
otesic <qkrcjffus@naver.com>
2022-10-12 20:51:27 +0900

    katalk final

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+import requests
+from flask import Flask, render_template, request, json, redirect, flash
 from For_Disabled_Sign_Lang.slanguage.slanguage import SLanguageService
+from For_Disabled_Sign_Lang.bus.bus import BusService
 
 app = Flask(__name__)
+app.secret_key = 'asdf'
 sLanguageService = SLanguageService()
+busService = BusService()
 
 
 # 인덱스화면 연결
@@ -20,7 +24,8 @@ def sindex():
 
 @app.route('/bindex')
 def bindex():
-    return render_template('bindex.html')
+    res = busService.get_all()
+    return render_template('bindex.html', res=res)
 
 
 # 상세 수어 페이지연결
@@ -29,6 +34,64 @@ def search():
     title = request.args.get('title')
     result = sLanguageService.get_by_title()
     return render_template('search.html', result=result, title1=title)
+
+
+# 카톡 테스트
+@app.route('/kakao')
+def kakao():
+    kakao = request.args.get('kakao')
+    print(kakao)
+    data2 = sLanguageService.get_all()
+    data3 = data2
+    list = []
+    for i in data3:
+        list.append(i["title"])
+
+    if kakao in list:
+        # 카카오톡 메시지 API
+        url = "https://kauth.kakao.com/oauth/token"
+        data = {
+            "grant_type": "authorization_code",
+            "client_id": "8942390846940f8918570a9c142941a3",
+            "redirect_url": "http://127.0.0.1:5000/sindex",
+            "code": "e3L2uh4k2tfrdjgwRAdzaSq8-uNPjOB1NYB5PC-gYXTNDz7SkV8yEsnbYBaNEYsVHY5Ftgo9dRsAAAGDyxQVCQ"
+        }
+        response = requests.post(url, data=data)
+        tokens = response.json()
+        # print(tokens)
+
+        # kakao_code.json 파일 저장
+        with open("slanguage/kakao_code.json", "w") as fp:
+            json.dump(tokens, fp)
+
+        url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+
+        # 사용자 토큰
+        headers = {
+            "Authorization": "Bearer " + 'Tr2xtJhc9KjZXiojpG_kWgkNVVrkhi5iVkV4WXnaCilv1AAAAYPLVL6Y'
+        }
+
+        data = {
+            "template_object": json.dumps({"object_type": "text",
+                                           "text": kakao,
+                                           # "text": data4,
+                                           "link": {'web_url': 'http://127.0.0.1:5000/search?title=' + kakao}
+                                           })
+        }
+
+        response = requests.post(url, headers=headers, data=data)
+        print(response.status_code)
+        if response.json().get('result_code') == 0:
+            print('메시지를 성공적으로 보냈습니다.')
+            flash('전송 성공')
+        else:
+            print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json()))
+
+    else:
+        print('검색오류')
+        flash("전송 실패")
+
+    return redirect('/sindex')
 
 
 if __name__ == '__main__':
---
+++ bus/bus.py
@@ -0,0 +1,32 @@
+import json
+
+import requests
+import xmltodict
+from bs4 import BeautifulSoup
+
+
+# Service
+class BusService:
+
+    # 버스 인덱스 화면에서 수어 리스트 뿌려주는 함수
+    def get_all(self):
+        url = 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?serviceKey=%2FUSr%2Fec%2F4ywJMNg1N9o%2B2cn%2FWo0MfbowBcc5vnFaY0S9jl3UlGVoJaASiHDTTEu%2Fdyb5iN%2BcHMIBOnYfdgb34A%3D%3D'
+        #url = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=lngSweqj5VjrFqcUQWLMyvM6OKEME1J3'
+        ### xml 시도 ###
+        # html = requests.get(url).text  # url에 요청
+        # print(html)
+        # root = BeautifulSoup(html, 'lxml-xml')
+
+        ### json 시도 ###
+        html1 = requests.get(url).text
+        html2 = xmltodict.parse(html1)
+        res1 = json.dumps(html2)
+        res2 = json.loads(res1)
+        print(res2)
+        return res2
+
+
+    def get_by_bus(self):
+
+
+        pass
